08d1f7d40e597d296cdae8524a4164384d9fcd8d
Alla Gasmi <alla.gasmi@gmail.insat.ucar.tn>
2025-04-04 19:28:18 +0100

    Modif dans ex5/home.php

--- ex5/home.php
+++ ex5/home.php
@@ -1,8 +1,12 @@
 <?php
 include("header.php");
 ?>
-<div>
-    <h1>Hello PHP lovers, welcome to your administration plateforme</h1>
+<br>
+<br>
+<div class="container-fluid">
+    <h1 class="text-center">Welcome to the Students Management System</h1>
+    <p class="text-center">This is a simple web application to manage students.</p>
+    <p class="text-center">You can  <a href="login.php">log in </a> or <a href="signin.php">sign in</a> to access the system.</p>
 </div>
 <?php
 
