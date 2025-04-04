97357909695a86e147c553db3d6fe3852f9e996d
Alla Gasmi <alla.gasmi@gmail.insat.ucar.tn>
2025-04-04 20:52:35 +0100

    changement de la page signin.php

--- ex5/signin.php
+++ ex5/signin.php
@@ -1,27 +1,41 @@
 <?php
 include("header.php");
 ?>
-    <div>
-        <form action="handel.php" method="post">
-            <input type="hidden" name="form_type" value="signup">
-            <label for="username">Username: </label>
-            <input type="text" name="username" id="username">
-            <br>
-            <label for="email">Email:  </label>
-            <input type="email" name="email" id="email">
-            <br>
-            <label for="role">Role: </label>
-            <label>
-            <input type="radio" name="role" value="user"> User
-            </label>
-            <label>
-            <input type="radio" name="role" value="admin"> Admin
-            </label>
-            <br>
-            <label for="password">Password:  </label>
-            <input type="password" name="password" id="password">
-            <button type="submit" >Sign up</button>
-        </form>
+    
+    <link rel="stylesheet" href="style.css">
+    <div class="conteneur">
+    <div class="container">
+    <form class="row g-3" action="handel.php" method="POST">
+        <input type="hidden" name="form_type" value="signup">
+        <div class="col-12">
+            <label for="inputUserName" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" name="username">
+        </div>
+        <div class="col-md-12">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+        <div class="col-md-12">
+            <label for="inputPassword4" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+       
+        <div class="col-md-12">
+            <label for="inputUserType" class="form-label">Sign up  as:</label>
+            <select id="inputUserType" class="form-select" name="inputUserType">
+                <option name="role" value="admin">Admin</option>
+                <option name="role" value="user" selected>User</option>
+            </select>
+        </div>
+        <div class="col-md-5">
+        </div>
+        <div class="col-md-4">
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+        </div>
+        <div class="col-md-4">
+        </div>
+    </form>
+    </div>
     </div>
 <?php
 include("footer.php");
